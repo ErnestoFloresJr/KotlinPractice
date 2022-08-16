2459b151240df3cba806016ac4f590e859514dfc
ErnestoFloresJr <63756603+ErnestoFloresJr@users.noreply.github.com>
2022-08-16 12:39:11 +0800

    Delegation using map
    property delegation using map as the helper

---
+++ PropertyDelegationUsingMap.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val obj = PropertyDelegationUsingMap(mutableMapOf("prop1" to "Rimuru Tempest", "prop2" to "Veldora Tempest"))
+    println("Property1: ${obj.prop1}")
+    println("Property2: ${obj.prop2}")
+}
+class PropertyDelegationUsingMap(private var map: MutableMap<String, String>) {
+    var prop1: String by map
+    var prop2: String by map
+}
